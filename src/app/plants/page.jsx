fe0c0a18de8d6f6d65505688bacd8a8391dc62dc
yxiuhli <luctr1509@gmail.com>
2024-05-21 16:35:30 +0700

    Merge filter to main

--- src/app/plants/page.jsx
+++ src/app/plants/page.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import ProductsPanel from "@/components/productsPanel/ProductsPanel";
 import { getPlants } from "@/lib/data";
 import SortMenu from "@/components/sortMenu/SortMenu";
+import Filters from "@/components/filters/Filters";
 
 const PlantsPage = async () => {
   const plants = await getPlants();
@@ -15,7 +16,7 @@ const PlantsPage = async () => {
         <SortMenu/>
       </div>
       <div className="flex gap-10 px-10 mt-4">
-        <div className="w-1/5 bg-blue-300">Filter</div>
+        <div className="w-1/5 bg-blue-300"><Filters/></div>
         <div className="w-4/5 bg-yellow-300">
           <ProductsPanel products={plants} />
         </div>
---
+++ src/components/filters/Filters.jsx
@@ -0,0 +1,133 @@
+"use client";
+import React, { useState } from "react";
+import {
+  Card,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Chip,
+  Collapse,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Checkbox,
+} from "@mui/material";
+import {
+  ChevronUpIcon,
+  ChartBarIcon,
+  WrenchIcon,
+  CircleStackIcon,
+} from "@heroicons/react/24/solid";
+
+const Filters = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
+
+  return (
+    <List>
+      <Accordion open={open === 1} onClick={() => handleOpen(1)}>
+        <AccordionSummary expandIcon={<ChevronUpIcon className="h-5" />}>
+          <ListItemIcon>
+            <ChartBarIcon className="h-5 ml-5 mt-1" />
+          </ListItemIcon>
+          <ListItemText primary="Plant Size" />
+          <Chip
+            label="3"
+            className="rounded-full bg-blue-500 text-white mr-2"
+            size="small"
+          />
+        </AccordionSummary>
+        <AccordionDetails>
+          <List component="div" disablePadding>
+            <ListItem>
+              <Checkbox label="size" />
+              <ListItemText primary="Small" />
+            </ListItem>
+            <ListItem>
+              <Checkbox label="size" />
+              <ListItemText primary="Medium" />
+            </ListItem>
+            <ListItem>
+              <Checkbox label="size" />
+              <ListItemText primary="Large" />
+            </ListItem>
+          </List>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion open={open === 2} onClick={() => handleOpen(2)}>
+        <AccordionSummary expandIcon={<ChevronUpIcon className="h-5" />}>
+          <ListItemIcon>
+            <WrenchIcon className="h-5 ml-5 mt-1" />
+          </ListItemIcon>
+          <ListItemText primary="Dificulty" />
+          <Chip
+            label="3"
+            className="rounded-full bg-blue-500 text-white mr-2"
+            size="small"
+          />
+        </AccordionSummary>
+        <AccordionDetails>
+          <List component="div" disablePadding>
+            <ListItem>
+              <Checkbox label="diff" />
+              <ListItemText primary="No-Fuss" />
+            </ListItem>
+            <ListItem>
+              <Checkbox label="diff" />
+              <ListItemText primary="Easy" />
+            </ListItem>
+            <ListItem>
+              <Checkbox label="diff" />
+              <ListItemText primary="Moderate" />
+            </ListItem>
+          </List>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion open={open === 3} onClick={() => handleOpen(3)}>
+        <AccordionSummary expandIcon={<ChevronUpIcon className="h-5" />}>
+          <ListItemIcon>
+            <CircleStackIcon className="h-5 ml-5 mt-1" />
+          </ListItemIcon>
+          <ListItemText primary="Expandable Item" />
+          <Chip
+            label="5"
+            className="rounded-full bg-blue-500 text-white mr-2"
+            size="small"
+          />
+        </AccordionSummary>
+        <AccordionDetails>
+          <List component="div" disablePadding>
+            <ListItem>
+              <Checkbox label="price" />
+              <ListItemText primary="Under $50" />
+            </ListItem>
+            <ListItem>
+              <Checkbox label="price" />
+              <ListItemText primary="$50-$100" />
+            </ListItem>
+            <ListItem>
+              <Checkbox label="price" />
+              <ListItemText primary="$100-$200" />
+            </ListItem>
+            <ListItem>
+              <Checkbox label="price" />
+              <ListItemText primary="$200-$300" />
+            </ListItem>
+            <ListItem>
+              <Checkbox label="price" />
+              <ListItemText primary="$300 & Above" />
+            </ListItem>
+          </List>
+        </AccordionDetails>
+      </Accordion>
+    </List>
+  );
+};
+
+export default Filters;
